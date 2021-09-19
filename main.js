d86b8e4fb783663d7d2edb1c15529f819301b668
Zaynthecoder <68971876+Zaynthecoder@users.noreply.github.com>
2021-09-19 10:45:01 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function setDate(){
+    document.getElementById('date').innerHTML = new Date().toDateString();
+}
+
+function twelveinfo() {
+    window.alert("'This is the iPhone 12 Pro. There are four models. iPhone 12 mini, iPhone 12, iPhone 12 Pro, and iPhone 12 Pro Max. iPhone 12s start at $700'");
+}
+
+function mbookairinfo() {
+    window.alert("'This is the M1 Macbook Air. It starts at $1000. It has the superfast Custom Designed Apple M1. Its nice'");
+
+}
+function ipadproinfo() {
+    window.alert("This is the iPad Pro. It starts at $800. It has the M1 Processor.");
+}
+
+function iwatchinfo() {
+    window.alert("This is the Apple Watch Series 6. It can tell you the time. read messages. Answer calls. And measure body stuff.");
+}
